691e18ffaccdaec72b3ad4305d49b916aa3258e4
Anirudh <anirudhpatnaik2804@gmail.com>
2023-08-11 20:37:40 +0530

    modified nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -9,7 +9,8 @@ const Nav = () => {
       <a href="#" onClick={() => activeNav('#')} className={nav === '#' ? "active" : ''}>Home</a>
       <a href="#about" onClick={() => activeNav('#about')} className={nav === '#about' ? 'active' : ''}>About</a>
       <a href="#skills" onClick={() => activeNav('#skills')} className={nav === '#skills' ? 'active' : ''}>Skills</a>
-      <a href="#projects" onClick={() => activeNav('#projects')} className={nav === '#projects' ? 'active' : ''}>Projects</a>
+      <a href="#projects" onClick={() => activeNav('#projects')} className={nav === '#projects' ? 'active' : ''}>Portfolio</a>
+      <a href="#contact" onClick={() => activeNav('#contact')} className={nav === '#contact' ? 'active' : ''}>Contact</a>
       
     </nav>
   )
