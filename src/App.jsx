cff1c80b5cdf96256290b6df486a0c1eb3364f70
Anirudh <anirudhpatnaik2804@gmail.com>
2023-08-12 18:03:29 +0530

    added backt_to_top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/header/header'
 import Nav from './components/nav/nav'
+import Scroll from './components/scrollToTop/scroll'
 import About from './components/about/about'
 import Skills from './components/skills/skills'
 import Projects from './components/projects/projects'
 import Contact from './components/contact/contact'
+
 
 
 const App = () => {
@@ -12,6 +14,7 @@ const App = () => {
     <>
         <Header />
         <Nav />
+        <Scroll />
         <About />
         <Skills />
         <Projects/>
--- src/components/scrollToTop/scroll.jsx
+++ src/components/scrollToTop/scroll.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import './scroll.css'
+import {AiOutlineArrowUp} from 'react-icons/ai'
 
-const scroll = () => {
+const Scroll = () => {
+  const [toTopButton, setToTopButton] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100){
+        setToTopButton(true)
+      }else{
+        setToTopButton(false)
+      }
+    })
+  },[])
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
   return (
-    <div>scroll</div>
+    <div>
+      {toTopButton && (
+        <a class="scroll__btn-top" onClick={scrollUp}  > <AiOutlineArrowUp id='scroll__up-icon'/>  </a>
+      )}
+    </div>
   )
 }
 
-export default scroll
+export default Scroll
