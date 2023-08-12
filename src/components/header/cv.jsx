80a901d32a25551ddbcdd2773c5a3e3f448c7cdf
Anirudh <anirudhpatnaik2804@gmail.com>
2023-08-12 10:38:12 +0530

    added scrollToTop

--- src/components/header/cv.jsx
+++ src/components/header/cv.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/cv.pdf'
 const cv = () => {
   return (
     <div className="cv">
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download = 'anirudh_patnaik_cv' className='btn'>Download CV</a>
         <a href="#about" className='btn btn-primary'>About</a>
     </div>
   )
